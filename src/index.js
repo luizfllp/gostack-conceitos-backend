984333e7f8a6aec595dcca398e88fe5f20fb8f1d
Luiz Fellype <luizfllp@pop-os.localdomain>
2021-03-02 17:40:57 -0300

    Middleware validando rota

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 const app = express();
 
 app.use(express.json());
 
 const projects = []
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    return next();
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID' });
+    }
+
+    return next();
+}
+
+app.use(logRequests);
+
 app.get('/projects', (request, response) => {
     const { title } = request.query;
-    const results = projects.filter(project => project.title.includes(title))
+    const results = title ? projects.filter(project => project.title.includes(title)) : projects;
     return response.json(results);
 });
 
@@ -21,7 +44,7 @@ app.post('/projects', (request, response) => {
     return response.json(project);
 });
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateProjectId, (request, response) => {
     const { id } = request.params;
     const { title, owner } = request.body;
     const projectIndex = projects.findIndex(project => project.id === id);
@@ -37,7 +60,7 @@ app.put('/projects/:id', (request, response) => {
     return response.json(project);
 });
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateProjectId, (request, response) => {
     const { id } = request.params;
 
     const projectIndex = projects.findIndex(project => project.id === id);
